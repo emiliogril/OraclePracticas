14e525989863478b4da88ab9e99599540c9d5934
Emilio Gril <emiliogril@gmail.com>
2022-12-11 12:28:48 -0300

    Desafio Oracle

---
+++ script.js
@@ -0,0 +1,22 @@
+let miNombre = "Barbara"
+console. log (miNombre)
+console.log (window)
+
+function mostrarMiNombre (){
+    console.log (miNombre)
+}
+
+mostrarMiNombre ()
+
+function mostrarMiApellido (){
+    var miApellido = "dos santos"
+    console. log (miApellido)
+}
+mostrarMiApellido ()
+
+if(true){
+    const animal = "perro" 
+    console.log(animal)
+    // animal = "gato"
+    // console.log(animal)
+}
